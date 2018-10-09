ebaa65c2b8fb1d397a0aaf88f69de29f932d5400
Manoj Kumar S <kumarmanoj1158@gmail.com>
2018-10-09 20:30:26 +0530

    fix s bug

--- src/sumOfProducts.js
+++ src/sumOfProducts.js
@@ -1,7 +1,7 @@
 export const sumOfProducts = (array1, array2) => {
-    let sum = 0;
-    array1.forEach((element, index) => {
-      sum += element * array2[index];
-    });
-    return sum;s
+  let sum = 0;
+  array1.forEach((element, index) => {
+    sum += element * array2[index];
+  });
+  return sum;
 };
