e4b26594e1ae46f8e65a59a880fc09f2c0cbacd3
Manoj Kumar S <kumarmanoj1158@gmail.com>
2018-10-09 20:06:42 +0530

    implement sum of products

--- src/sumOfProducts.js
+++ src/sumOfProducts.js
@@ -1,3 +1,7 @@
-export const sumOfProducts = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const sumOfProducts = (array1, array2) => {
+    let sum = 0;
+    array1.forEach((element, index) => {
+      sum += element * array2[index];
+    });
+    return sum;s
 };
